3083594dd9d896483f1608729605ddfb378c8dcd
icedance <961590104@qq.com>
2023-05-23 05:58:38 +0000

    update

--- src/app/tool-box/tool-box-routing.module.ts
+++ src/app/tool-box/tool-box-routing.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WikiComponent } from '../wiki/wiki.component';
 
 const routes: Routes = [
   {
-    path: '', component: WikiComponent
-    // redirectTo: 'set',
-    // pathMatch: 'full'
+    path: '',
+    redirectTo: 'set',
+    pathMatch: 'full'
   },
-  // {
-  //   path: 'set', 
-  //   loadChildren: () => import('../tool-set/tool-set.module').then(m => m.ToolSetModule)
-  // },
-  // {
-  //   path: 'json',
-  //   loadChildren: () => import('../json/json.module').then(m => m.JsonModule)
-  // }
+  {
+    path: 'set', 
+    loadChildren: () => import('../tool-set/tool-set.module').then(m => m.ToolSetModule)
+  },
+  {
+    path: 'json',
+    loadChildren: () => import('../json/json.module').then(m => m.JsonModule)
+  }
 ];
 
 @NgModule({
