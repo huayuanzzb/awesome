02f3254d7830da25423fd90ebbbb9f4cf822c7aa
icedance <961590104@qq.com>
2023-05-29 00:51:26 +0000

    update

--- src/components/code/Json.vue
+++ src/components/code/Json.vue
@@ -6,8 +6,9 @@
     </el-col>
 
     <el-col :span="12">
-      <CodeBlock prismjs :code="after"></CodeBlock>
-      <!-- <el-input input-style="height: 100%" v-model="after" type="textarea" placeholder="Please input" /> -->
+      <!-- 貌似 vue3-code-block 有bug， after 有变化时， 代码高亮和行号插件都不能及时响应 -->
+      <CodeBlock v-if="after" prismjs prism-plugin lang="json" class="line-numbers" :code="after"></CodeBlock>
+      <CodeBlock v-else prismjs lang="html" :code="hint"/>
     </el-col>
   </el-row>
 </template>
@@ -15,19 +16,28 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
+import 'prismjs'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/components/prism-json';
+
+
 const before = ref('')
-const hint = "左侧填入 Json 字符串后，此处将显示格式化后的结果。"
-const after = ref(hint)
+const placeholder = "左侧输入json字符串，此处将自动展示格式化后的效果"
+const hint = ref(placeholder)
+const after = ref<string | null>(null)
 
 const onBeforeChange = () => {
   try {
     if (before.value == null || before.value == undefined || before.value.trim() == '') {
-      after.value = hint;
+      after.value = null;
+      hint.value = placeholder;
     } else {
       after.value = JSON.stringify(JSON.parse(before.value), null, 4);
     }
   } catch (error: any) {
-    after.value = error.message;
+    after.value = null;
+    hint.value = error.message;
   }
 }
 
