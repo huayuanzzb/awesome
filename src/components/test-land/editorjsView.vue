5f04873b56158ef0576ffcff6648744e31411b48
icedance <961590104@qq.com>
2023-06-06 02:56:33 +0000

    update editorjs

--- src/components/test-land/editorjsView.vue
+++ src/components/test-land/editorjsView.vue
@@ -5,8 +5,16 @@
 
 <script setup lang="ts">
 import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header'; 
+import SimpleImage from '@editorjs/simple-image';
+import Link from '@editorjs/link';
 
 const editor = new EditorJS({
-    holder: "editorjs"
+    holder: "editorjs",
+    tools: { 
+        header: Header, 
+        simpleImage: SimpleImage,
+        link: Link,
+  }, 
 });
 </script>
