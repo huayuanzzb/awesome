7495957963a2f0b1db91ac0b7350b8f1bd8bd31f
huayuanzzb <961590104@qq.com>
2024-04-03 08:42:35 +0000

    update

--- src/components/code/time/Time.vue
+++ src/components/code/time/Time.vue
@@ -1,36 +1,47 @@
 <template>
-  <el-descriptions :column="1" border>
-    <el-descriptions-item>
-      <template #label>
-        <div class="cell-item">Epoc</div>
-      </template>
-      {{ mills }}
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template #label>
-        <div class="cell-item">Timestamp</div>
-      </template>
-      {{ now }}
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template #label>
-        <div class="cell-item">Timezone</div>
-      </template>
-      {{ Intl.DateTimeFormat().resolvedOptions().timeZone }}
-    </el-descriptions-item>
-  </el-descriptions>
+  <el-row :gutter="5">
+    <el-col :span=12>
+      <el-descriptions :column="1" border>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">epoch</div>
+          </template>
+          {{ epoch }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">Timestamp</div>
+          </template>
+          {{ now }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template #label>
+            <div class="cell-item">Timezone</div>
+          </template>
+          {{ Intl.DateTimeFormat().resolvedOptions().timeZone }}
+        </el-descriptions-item>
+      </el-descriptions>
+    </el-col>
+    <el-col :span=12>
+      <el-input ref="beforeInput" v-model="before"></el-input>
+    </el-col>
+  </el-row>
+
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 
 const now = ref<Date>()
-const mills = ref<number>()
+const epoch = ref<number>()
+const before = ref<string>()
+const beforeInput = ref()
 
 onMounted(() => {
+  beforeInput?.value?.focus()
   setInterval(() => {
     now.value = new Date()
-    mills.value = now.value.getTime()
+    epoch.value = now.value.getTime()
   }, 1)
 })
 
