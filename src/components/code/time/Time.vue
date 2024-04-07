af741bbcaee1fba4ed20be2f32eb9393efe2a7ab
huayuanzzb <961590104@qq.com>
2024-04-07 09:18:26 +0000

    update

--- src/components/code/time/Time.vue
+++ src/components/code/time/Time.vue
@@ -4,31 +4,32 @@
     {{ '>>' }}
     <el-input ref="afterInput" v-model="after">
       <template #append>
-        <el-select v-model="selectFormat" placeholder="Select a format" style="width: 240px" @change="onSelectFormat()">
+        <el-select v-model="selectFormat" placeholder="Select a format" style="width: 320px" @change="covert">
           <el-option v-for="f in formats" :label="f" :value="f" />
-          <el-option label="Custom..." value="custom" />
+          <template #footer>
+            <el-button v-if="!isAdding" text bg size="small" @click="onAddOption">
+              Add an format
+            </el-button>
+            <template v-else>
+              <el-form ref="customFormatFormRef" :label-position="'top'" :rules="customFormatFormRules"
+                :model="customFormatForm">
+                <el-form-item label="New Format">
+                  <el-input v-model="customFormatForm.customFormat" />
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary" size="small" @click="saveCustomFormat(customFormatFormRef)">
+                    confirm
+                  </el-button>
+                  <el-button size="small" @click="clear">cancel</el-button>
+                </el-form-item>
+              </el-form>
+            </template>
+          </template>
         </el-select>
       </template>
     </el-input>
-    <el-link>
-      <el-icon>
-        <QuestionFilled />
-      </el-icon>
-    </el-link>
 
-    <el-dialog v-model="dialogVisible" width="500" title="Tips">
-      <span>Use dayjs to format date</span>
-      <el-input v-model="customFormat"></el-input>
-      <el-text type="danger" v-for="e in customFormatError">{{ e }}</el-text>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button @click="dialogVisible = false">Cancel</el-button>
-          <el-button type="primary" @click="saveCustomFormat()">
-            Confirm
-          </el-button>
-        </div>
-      </template>
-    </el-dialog>
+
   </div>
 
   <el-descriptions :column="1" border>
@@ -55,13 +56,17 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
 import { dayjs } from 'element-plus'
 import {
   QuestionFilled
 } from "@element-plus/icons-vue";
 
 const CUSTOM_DATETIME_FORMAT_KEY = 'custom-datetime-formats'
+const DEFAULT_FORMATS = [
+  'YYYY-MM-DD hh:mm:ss.SSS'
+]
 
 const now = ref<Date>()
 const epoch = ref<number>()
@@ -70,12 +75,17 @@ const after = ref<string>()
 const beforeInput = ref()
 const afterInput = ref()
 const selectFormat = ref('YYYY-MM-DD hh:mm:ss.SSS')
-const customFormat = ref('')
-const customFormatError = ref<string[]>([])
-const dialogVisible = ref(false)
-const formats = ref([
-  'YYYY-MM-DD hh:mm:ss.SSS'
-])
+const isAdding = ref(false)
+const customFormatForm = reactive({
+  customFormat: ''
+})
+const customFormatFormRef = ref<FormInstance>()
+const customFormatFormRules = reactive<FormRules>({
+  customFormat: [
+    { required: true, message: 'Please input custom format', trigger: 'blur' }
+  ]
+})
+const formats = reactive<string[]>([])
 
 onMounted(() => {
   beforeInput?.value?.focus()
@@ -85,27 +95,47 @@ onMounted(() => {
     now.value = new Date()
     epoch.value = now.value.getTime()
   }, 1)
+  computeForamts()
 })
 
-const onSelectFormat = () => {
-  dialogVisible.value = selectFormat.value == 'custom'
+const computeForamts = () => {
+  formats.length = 0
+  DEFAULT_FORMATS.forEach(i => formats.push(i))
+  getCachedFormats().forEach(i => formats.push(i))
 }
-const saveCustomFormat = () => {
-  // try {
-  //   dayjs(new Date()).format(customFormat.value)
-  // } catch {
-  //   customFormatError.value.push('Invalid format')
-  // }
-  // let origin = localStorage.getItem(CUSTOM_DATETIME_FORMAT_KEY)
-  // let originSet = new Set<string>()
-  // if (origin) {
-  //   let dataArray = JSON.parse(origin) as string[];
-  //   originSet = new Set(dataArray)
-  // }
-  // originSet.add(customFormat.value)
-  // localStorage.setItem(CUSTOM_DATETIME_FORMAT_KEY, JSON.stringify(Array.from(originSet)))
-  // customFormat.value = ''
-  // dialogVisible.value = false
+
+const saveCustomFormat = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      var originSet = getCachedFormats()
+      originSet.add(customFormatForm.customFormat)
+      localStorage.setItem(CUSTOM_DATETIME_FORMAT_KEY, JSON.stringify(Array.from(originSet)))
+      isAdding.value = false
+      customFormatForm.customFormat = ''
+      computeForamts()
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+
+const getCachedFormats = () => {
+  var cached = localStorage.getItem(CUSTOM_DATETIME_FORMAT_KEY) || '[]'
+  var cachedSet = new Set<string>()
+  if (origin) {
+    let dataArray = JSON.parse(cached) as string[];
+    cachedSet = new Set(dataArray)
+  }
+  return cachedSet
+}
+
+const onAddOption = () => {
+  isAdding.value = true
+}
+const clear = () => {
+  customFormatForm.customFormat = ''
+  isAdding.value = false
 }
 const covert = () => {
   formatDate(new Date(Number(before.value)))
